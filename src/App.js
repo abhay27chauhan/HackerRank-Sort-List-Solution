f0f3e5d3b7d380e6e8dfde2e06067c33a72b464c
Abhay <chauhan27.abhay@gmail.com>
2020-12-29 16:56:10 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -8,15 +8,39 @@ const title = "Sorting Articles";
 
 function App({articles}) {
 
+    const[state, setState] = useState([]);
+
+    useEffect(() => {
+        setState(articles);
+    }, [articles])
+
+    const sortByDate = () => {
+        let data = [...articles];
+        data.sort((a, b) => {
+            let dateA = new Date(a.date);
+            let dateB = new Date(b.date);
+
+            return dateB-dateA;
+        })
+
+        setState(data)
+    }
+
+    const sortByUpvotes = () => {
+        let data = [...articles];
+        data.sort((a, b) => b.upvotes-a.upvotes);
+        setState(data)
+    }
+
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button onClick={sortByUpvotes} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+                <button onClick={sortByDate} data-testid="most-recent-link" className="small">Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={state}/>
         </div>
     );
 
